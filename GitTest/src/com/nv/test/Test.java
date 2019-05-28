93c1fca31df1f72d343deadce7045b872f977aea
Nav <Nav@DESKTOP-527VT3F>
2019-05-28 09:33:22 +0530

    updated method

--- GitTest/src/com/nv/test/Test.java
+++ GitTest/src/com/nv/test/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello how are u");
 	}
-
+	public void m1()
+	{
+		System.out.println("this is new implemented logic");
+	}
 }
